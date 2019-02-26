c440c43864455d188c633e56ee10b8c47903fb08
TEH <Haire_thomas@yahoo.com>
2019-02-26 03:54:36 -0500

    My Site
    Still under maintenance

---
+++ assets/js/teh.js
@@ -0,0 +1,11 @@
+var text = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 40,
+    typeSpeed: 80,
+    backDelay: 700,
+    backSpeed: 100,
+    showCursor: true,
+    cursorChar: '[]',
+    loop: true,
+    loopCount: 1
+  });
