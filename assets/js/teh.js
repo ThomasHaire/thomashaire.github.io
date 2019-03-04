c56c5523871d0189d5b68388b1f39830187ebc62
TEH <Haire_thomas@yahoo.com>
2019-03-03 23:32:37 -0500

    Update
    CSS- img hover effects
    CSS- Colors

--- assets/js/teh.js
+++ assets/js/teh.js
@@ -1,11 +1,10 @@
 var text = new Typed('#typed', {
-    stringsElement: '#typed-strings',
-    typeSpeed: 40,
-    typeSpeed: 80,
-    backDelay: 700,
-    backSpeed: 100,
-    showCursor: true,
-    cursorChar: '[]',
-    loop: true,
-  });
+  stringsElement: '#typed-strings',
+  typeSpeed: 80,
+  backDelay: 700,
+  backSpeed: 100,
+  showCursor: true,
+  cursorChar: '.',
+  loop: true
+});
 //  loopCount: 1
