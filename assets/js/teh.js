ff53ebf8fd1366360ca7267321dcf5810ca403eb
TEH <Haire_thomas@yahoo.com>
2019-03-21 13:52:25 -0400

    Update
    CSS
    HTML
    JS

--- assets/js/teh.js
+++ assets/js/teh.js
@@ -2,13 +2,20 @@ $(document).ready(function(){
     $(this).scrollTop(0);
 });
 
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
+
 var text = new Typed('#typed', {
   stringsElement: '#typed-strings',
   typeSpeed: 80,
   backDelay: 700,
   backSpeed: 100,
   showCursor: true,
-  cursorChar: '|',
+  cursorChar: '.',
   loop: true,
   loopCount: 1
 });
